692080063680a79a9ad37488b2bde3e8832d07c1
Orr Peer <h3123456@gmail.com>
2025-01-31 19:27:26 +0200

    Added ability to input grinder names in to the DB

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Data.SQLite;
-using CoffeeDocs;
-using System.Xml.Linq;
-
+﻿using System.Text;
 
 namespace CoffeeLogger
 {
@@ -13,19 +6,53 @@ namespace CoffeeLogger
     {
         public SQLController db;
 
+        public static Program instance;
+
+        private static void Main()
+        {
+            instance = new Program();
+        }
+
         public Program()
         {
-            db = new SQLController();
-            db.Activate();
             Init();
-
         }
 
         private void Init()
         {
-
+            db = new SQLController();
+            db.Activate();
+            ConsoleReader();
         }
 
+        private async Task ConsoleReader()
+        {
+            string input = string.Empty;
+            while (true)
+            {
+                Console.Write("\n>");
+                input = Console.ReadLine().Trim().ToLower();
+                Console.WriteLine("");
+
+                switch (input)
+                {
+                    case "newgrinder":
+                        await AddNewGrinderFromConsole();
+                        break;
+                    case "cleardb":
+                        ClearDB(); 
+                        break;
+
+                    case "stop" or "quit":
+                        return;
+
+                    default:
+                    case null or "":
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+            }
+        }
 
         private void ClearDB()
         {
@@ -33,19 +60,81 @@ namespace CoffeeLogger
             db.ClearDB();
         }
 
-        private void AddNewGrinder(string name)
+        private async Task<string?> ReadWithEsc()
+        {
+            StringBuilder input = new StringBuilder();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+                    ConsoleKey rawKey = keyInfo.Key;
+                    Char keyChar = keyInfo.KeyChar;
+
+                    if (rawKey == ConsoleKey.Escape)
+                    {
+                        return null;
+                    }
+                    if (rawKey == ConsoleKey.Enter)
+                    {
+                        return input.ToString();
+                    }
+                    if (rawKey == ConsoleKey.Backspace && input.Length != 0)
+                    {
+                        input.Remove(input.Length - 1, 1);
+                        Console.Write("\b \b"); //wat?
+                        continue;
+                    }
+                    if (!char.IsControl(keyChar))
+                    {
+                        input.Append(keyChar);
+                        Console.Write(keyChar);
+                    }
+
+                    
+                }
+            }
+        }
+
+
+        private async Task AddNewGrinderFromConsole()
+        {
+            while (true)
+            {
+                Console.Write("\nEnter grinder name:\n>");
+                string? name = await ReadWithEsc();
+                if (name == null)
+                {
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(name) || !name.Any(char.IsAsciiLetter))
+                {
+                    Console.WriteLine("Invalid name\n");
+                    continue;
+                }
+                if (AddNewGrinder(name))
+                {
+                    return;
+                }
+
+            }
+        }
+
+        private bool AddNewGrinder(string name)
         {
             string grinderName = name;
             string[] grinderNamesOnDB = db.GetGrinderNames();
 
             if (grinderNamesOnDB.Contains(grinderName))
             {
-                Console.WriteLine("Grinder name is taken on the DB");
-                return;
+                Console.WriteLine("\n\nGrinder name is taken on the DB\n");
+                return false;
             }
 
             db.AddGrinder(grinderName);
-            Console.WriteLine($"Grinders on DB: {string.Join(", ", db.GetGrinderNames())}");
+            Console.WriteLine($"\n\nGrinders on DB: {string.Join(", ", db.GetGrinderNames())}");
+            return true;
         }
 
         private void AddNewBrewer(string name)
